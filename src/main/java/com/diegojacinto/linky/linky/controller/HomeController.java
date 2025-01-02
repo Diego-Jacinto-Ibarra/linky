86b6545e3c535e5388f744563310841340d6ac53
Diego Jacinto <diego.jacinto.ibarra@gmail.com>
2025-01-02 16:29:17 -0600

    Rename showHomePage method to home for clarity

--- src/main/java/com/diegojacinto/linky/linky/controller/HomeController.java
+++ src/main/java/com/diegojacinto/linky/linky/controller/HomeController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String showHomePage() {
+    public String home() {
         return "home";
     }
+
 }
